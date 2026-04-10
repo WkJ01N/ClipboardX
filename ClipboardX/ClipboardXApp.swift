3117a805cad9800c66fb7b03aa85ac3b8c28513b
Rain Walker <walkersj01n@outlook.com>
2026-04-10 22:49:22 +0800

    Initial Commit

---
+++ ClipboardX/ClipboardXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardXApp.swift
+//  ClipboardX
+//
+//  Created by Rain Walker on 2026/4/10.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
